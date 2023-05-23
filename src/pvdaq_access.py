0536c7ba7f3a3c02ff6e78570b1f2a688d7ed5d7
Robert White <Robert.White@nrel.gov>
2023-05-23 15:26:44 -0600

    Added concatenation of downloaded files
    Added as part of process. After all files are downloaded they get concatenated.

--- src/pvdaq_access.py
+++ src/pvdaq_access.py
@@ -24,6 +24,7 @@ import boto3
 import botocore
 import argparse
 from botocore.handlers import disable_signing
+import pandas as pd
 
 #---------------------------------------------------------------------------   
 def downloadData(system_id, path, file_type='csv'):
@@ -61,6 +62,41 @@ def downloadData(system_id, path, file_type='csv'):
 
 
 #---------------------------------------------------------------------------   
+def concatenateData(system_id, path):
+    '''
+    Method to merge the multiple files coming in from OEDI
+    Parameters:
+    -----------------------
+    system_id : str - system id value found from query of OEDI PVDAQ queue 
+    of available .
+    path : str - local system location files are to be stored in.
+    
+    Returns
+    -----------------------
+    void
+    
+    '''
+    dfs = []
+    #get list of files in directory
+    file_list=os.listdir(path)
+    column_name = 'sensor_name'
+    #Build a dataframe from current file
+    print ("Starting data extraction")
+    for file in file_list:
+        print("Extracting file " + file)
+        df_file= pd.read_csv(path + '/' + file)
+        dfs.append(df_file)
+         
+    #Build the master data frame from the assembled individual frames.
+    print ("Concatenating all files")
+    df = pd.concat(dfs, ignore_index=True)
+    target_outputfile = path + "/system_" + system_id + "_data.csv"
+    print ("File is " + target_outputfile)
+    df.to_csv(target_outputfile, sep=",", index=False)
+    return
+
+
+#---------------------------------------------------------------------------   
 #---------------------------------------------------------------------------   
 if __name__ == '__main__':
     print (" ..: Starting data access script for PVDAQ OEDI datasets :..")
@@ -71,12 +107,14 @@ if __name__ == '__main__':
     parser.add_argument('-path', type=str,  help="Location to store files locally")
     parser.add_argument('-parquet', help="Access parquet files (default is .csv)", action="store_true")
     args = parser.parse_args()
-    
+       
     if args.system:
         if args.parquet:
             downloadData(args.system, args.path, file_type='parquet')
         else:
             downloadData(args.system, args.path)
+            #Create single file from data
+            concatenateData(args.system, args.path)            
     else:
         print('Missing system_id, Exiting.')
     
