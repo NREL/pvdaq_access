2db7a8d96ff46757a620835a579a29938af9ce66
Shiru <silvana.ovaitt@nrel.gov>
2025-04-09 17:16:06 -0600

    remove versioneer / new versioning system
    versioneer is causing issues with newer python releases ~ updated to new standard used elsewhere in our codes (bifacial_radiance, etc)

--- pvdaq_access/__init__.py
+++ pvdaq_access/__init__.py
@@ -1,3 +1,13 @@
+try:
+    from importlib.metadata import PackageNotFoundError, version
+except ImportError:
+    # for python < 3.8 (remove when dropping 3.7 support)
+    from importlib_metadata import PackageNotFoundError, version
+
+try:
+    __version__ = version(__package__)
+except PackageNotFoundError:
+    __version__ = "0+unknown"
+
+
 from pvdaq_access.main import downloadSolarPrizeData, downloadData, concatenateData
-from . import _version
-__version__ = _version.get_versions()['version']
--- setup.py
+++ setup.py
@@ -31,9 +31,8 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    # version='0.3.4',
-    version=versioneer.get_version(),
-    cmdclass=versioneer.get_cmdclass(),
+    use_scm_version={"fallback_version":"0.0.1"},
+
 
     description='Functions to interface with PVDAQ datasets',
     long_description=long_description,
